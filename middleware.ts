1ab6b67b43a3256695d39897c9f559cda77a7653
Instrutor Jackson <inst.jackson@gmail.com>
2026-03-30 01:07:55 -0300

    fix: pass NEXTAUTH_SECRET explicitly to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 // middleware.ts
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+})
 
 export const config = {
   matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)'],
